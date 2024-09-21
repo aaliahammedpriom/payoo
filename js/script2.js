fb29a0c3cbbfe407cbd164b159a969315ef17f58
aaliahammedpriom <aaliahammedpriom66@gmail.com>
2024-09-22 00:43:43 +0600

    transaction add

--- js/script2.js
+++ js/script2.js
@@ -1,14 +1,24 @@
-// confirm button event
-// document.getElementById('confirm-js').addEventListener('click',function(event){
-//     event.preventDefault();
-//     alert('Please Select What You Want !')
-// })
-
 // add money form show
 document.getElementById('add-money-js').addEventListener('click', function(event){
     // 
     document.getElementById('add-money-form-js').style.display = "block";
-    document.getElementById('cash-out-form-js').style.display = "";   
+    document.getElementById('cash-out-form-js').style.display = ""; 
+    document.getElementById('transaction-history-js').style.display = "";  
+})
+
+// cash out form show
+document.getElementById('cash-out-money-js').addEventListener('click', function(event){
+    //
+    document.getElementById('add-money-form-js').style.display = "";
+    document.getElementById('cash-out-form-js').style.display = "block";
+    document.getElementById('transaction-history-js').style.display = "";
+})
+// transaction history show
+document.getElementById('transaction-history-js').addEventListener('click', function(event){
+    //
+    document.getElementById('add-money-form-js').style.display = "";
+    document.getElementById('cash-out-form-js').style.display = "";
+    document.getElementById('transaction-history-js').style.display = "block";
 })
 
 //Add Money event
@@ -17,24 +27,23 @@ document.getElementById('add-money-confirm-js').addEventListener('click',functio
     // event preventing
     event.preventDefault();
 
-    if(document.getElementById('add-money-pin-js').value == 1234){
+    if(document.getElementById('add-money-pin-js').value == 1){
         // update blance event
-        document.getElementById('blance-js').innerHTML = parseFloat(document.getElementById('add-money-amount-js').value) + parseFloat(document.getElementById('blance-js').innerHTML); 
+        document.getElementById('blance-js').innerText = parseFloat(document.getElementById('blance-js').innerText) + parseFloat(document.getElementById('add-money-amount-js').value);  
         
+        //transaction history
+        // document.getElementById('transaction-section-js').innerText = (document.createElement('p').innerText = "Add Money: " + parseFloat(document.getElementById('add-money-amount-js').value) + " New Blance: " + document.getElementById('blance-js').innerText);
+        const p = document.createElement('p');
+        p.innerText = "Added : " + parseFloat(document.getElementById('add-money-amount-js').value) + " New Blance : " + document.getElementById('blance-js').innerText;
+        document.getElementById('transaction-section-js').appendChild(p);
     }
     else{
         alert("Wrong PIN !");
+        document.getElementById('transaction-section-js').innerText = (document.createElement('p').innerText = "Transaction Fail . Wrong PIN !");
     }
     
 })
 
-
-// cash out form show
-document.getElementById('cash-out-money-js').addEventListener('click', function(event){
-    
-    document.getElementById('cash-out-form-js').style.display = "block";
-    document.getElementById('add-money-form-js').style.display = "";
-})
 
 // cash out event
 document.getElementById('cash-out-confirm-js').addEventListener('click',function(event){
@@ -43,10 +52,14 @@ document.getElementById('cash-out-confirm-js').addEventListener('click',function
     if(document.getElementById('cash-out-pin-js').value == 1234){
         // update blance event
         document.getElementById('blance-js').innerHTML = parseFloat(document.getElementById('blance-js').innerHTML) - parseFloat(document.getElementById('cash-out-amount-js').value);
-     
-        
+
+        // transaction history
+        document.getElementById('transaction-section-js').innerHTML = (document.createElement('p').innerHTML = "Cash Out: " + parseFloat(document.getElementById('add-money-amount-js').value) + " New Blance: " + document.getElementById('blance-js').innerText);
     }
     else{
         alert("Wrong PIN !");
+        document.getElementById('transaction-section-js').innerHTML = (document.createElement('p').innerHTML = "Transaction Fail . Wrong PIN !");
     }
 })
+
+//transaction history event
